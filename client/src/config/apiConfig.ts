5e61ab6e6753f520dd951b56368b38c4e992ee94
okolo kamdilichukwu samuel <150658859+boi-network12@users.noreply.github.com>
2025-07-14 23:33:53 +0100

    refactor: update API_URL declaration and comment out unused code

--- client/src/config/apiConfig.ts
+++ client/src/config/apiConfig.ts
@@ -1,19 +1,19 @@
 // src/config/apiConfig.tsx
 
-let API_URL = 'https://maxproh.onrender.com/api'; 
+export const API_URL = 'https://maxproh.onrender.com/api'; 
 
-if (typeof window !== 'undefined') {
-  const hostname = window.location.hostname;
+// if (typeof window !== 'undefined') {
+//   const hostname = window.location.hostname;
 
-  if (
-    hostname === 'localhost' ||
-    hostname.startsWith('192.') || 
-    hostname.startsWith('127.') ||
-    hostname === '0.0.0.0'
-  ) {
-    API_URL = 'http://192.168.4.4:5000/api'; 
-  }
+//   if (
+//     hostname === 'localhost' ||
+//     hostname.startsWith('192.') || 
+//     hostname.startsWith('127.') ||
+//     hostname === '0.0.0.0'
+//   ) {
+//     API_URL = 'http://192.168.4.4:5000/api'; 
+//   }
 
-}
+// }
 
-export { API_URL };
+// export { API_URL };
